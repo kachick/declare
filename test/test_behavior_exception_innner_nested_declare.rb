00efe2ba274c7b5eef3abbb1e0b9f8e0c3eb8b8d
Kenichi Kamiya <kachick1@gmail.com>
2013-05-14 02:04:17 +0900

    Write test case for behavior of excuted

---
+++ test/test_behavior_exception_innner_nested_declare.rb
@@ -0,0 +1,38 @@
+# coding: us-ascii
+require 'test/unit'
+
+class Test_Decalre_Behavior_Exception_Inner_NestedDeclare < Test::Unit::TestCase
+  CODE = <<"EOD"
+class Person
+  
+  attr_reader :name, :birth
+  
+  def initialize(name)
+    @name = name
+    @birth = Time.now
+  end
+
+end
+
+app_dir = "#{File.expand_path('..', File.dirname(__FILE__))}"
+require app_dir + '/lib/declare/autorun'
+
+The Person.new('John') do |john|
+  can :name
+
+  The john.birth do
+    kind_of Time
+    raise Exception
+  end
+end
+EOD
+
+  def test_result
+    out = `ruby -w 2>&1 <<EOD
+#{CODE}
+EOD
+`
+    assert_equal(1, $?.exitstatus)
+    assert_match(/Exception/, out)
+  end
+end
---
+++ test/test_behavior_mixed.rb
@@ -0,0 +1,51 @@
+# coding: us-ascii
+require 'test/unit'
+
+class Test_Decalre_Behavior_Mixed < Test::Unit::TestCase
+  CODE = <<"EOD"
+class Person
+  
+  attr_reader :name, :birth
+  
+  def initialize(name)
+    @name = name
+    @birth = Time.now
+  end
+
+end
+
+app_dir = "#{File.expand_path('..', File.dirname(__FILE__))}"
+require app_dir + '/lib/declare/autorun'
+
+The Person.new('John') do |john|
+
+  can :name
+  can :birth
+  is_a Person
+  kind_of Object
+  
+  NOT 'Taro'
+  
+  The john.name do |name|
+    kind String
+    is 'Taro' # pass
+    is 'john' #fail
+    truthy name.kind_of?(String)
+    falthy name.match(/[1-9]/)
+  end
+
+  The john.birth do
+    kind_of Time
+  end
+
+end
+EOD
+
+  def test_result
+    out = `ruby -w 2>&1 <<EOD
+#{CODE}
+EOD`
+    assert_equal(2, $?.exitstatus)
+    assert_match(/3 scopes, 11 assertions, 2 failures/, out)
+  end
+end
